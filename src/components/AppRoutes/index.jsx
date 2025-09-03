2e85c28723cac252ee23b87395720ec2d23006b9
Nguyen Khanh Duy <21522003@gm.uit.edu.vn>
2025-09-03 11:27:33 +0700

    update router using hasing

--- src/components/AppRoutes/index.jsx
+++ src/components/AppRoutes/index.jsx
@@ -17,21 +17,19 @@ import Button from "../../pages/Button"
 
 function AppRoutes() {
     return (
-        <Router>
+        <HashRouter>
             <Navigation />
-            <HashRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/counter" element={<CounterApp />} />
-                    <Route path="/todo" element={<TodoList />} />
-                    <Route path="/products" element={<ProductList />} />
-                    <Route path="/comments" element={<CommentSystem />} />
-                    <Route path="/profile" element={<ProfileCard />} />
-                    <Route path="/weather" element={<WeatherApp />} />
-                    <Route path="/buttons" element={<Button />} />
-                </Routes>
-            </HashRouter>
-        </Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/counter" element={<CounterApp />} />
+                <Route path="/todo" element={<TodoList />} />
+                <Route path="/products" element={<ProductList />} />
+                <Route path="/comments" element={<CommentSystem />} />
+                <Route path="/profile" element={<ProfileCard />} />
+                <Route path="/weather" element={<WeatherApp />} />
+                <Route path="/buttons" element={<Button />} />
+            </Routes>
+        </HashRouter>
     )
 }
 
